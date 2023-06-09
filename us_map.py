1ea404bbd289b6f03b35a2aa0730c67b399f162e
Nobody01234567 <125545591+Nobody01234567@users.noreply.github.com>
2023-06-09 12:15:00 +0200

    Update us_map.py
    Check for Fire now directly incorporated in this code

--- us_map.py
+++ us_map.py
@@ -8,7 +8,8 @@ import geopandas as gpd
 from shapely.geometry import Point
 import earthpy as et
 import geodatasets
-from searchdata import checkForFire
+import arrow
+#from searchdata import checkForFire
 
 #weather_df = pd.read_csv('/Users/emilychristians/desktop/data/weather/city_info.csv')
 fire_df = pd.read_csv('/Users/emilychristians/desktop/ki_projekt/data/modis_2021_United_States.csv')
@@ -66,6 +67,39 @@ ax.set_ylim(y1, y2)
 
 ax.indicate_inset_zoom(ax, edgecolor="black")
 
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371  # Radius of the earth in kilometers
+    dLat = np.radians(lat2 - lat1)
+    dLon = np.radians(lon2 - lon1)
+    a = np.sin(dLat/2)**2 + np.cos(np.radians(lat1)) * np.cos(np.radians(lat2)) * np.sin(dLon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+    return R * c
+
+def load_city_data(city_id):
+    fpath = '/users/emilychristians/desktop/ki_projekt/data/weather/' + str(city_id) + '.csv'
+    return pd.read_csv(fpath)
+
+def load_fire_data():
+    return pd.read_csv('/users/emilychristians/desktop/ki_projekt/data/modis_2021_United_States.csv')
+
+def checkForFire(lat, lon, date, fire_df):
+    fire_df['distance'] = haversine(lat, lon, fire_df['latitude'], fire_df['longitude'])
+    closest_fire = fire_df.loc[fire_df['distance'].idxmin()]
+    closest_dist = closest_fire['distance']
+    closest_date = arrow.get(closest_fire['acq_date'])
+    #delta = abs(closest_date - arrow.get(date))
+    if closest_dist < 20:# and delta.days < 10:
+        message = 'There has been a fire in this area. It occurred on ' + closest_fire['acq_date'] + ' and was ' + str(closest_dist) + ' km away from the specified coordinates. Coordinates: ' + str(closest_fire['latitude']) + ',' + str(closest_fire['longitude'])
+        print(message)
+        x = np.arange(-120, 65, 0.05)
+        #Adding text inside a rectangular box by using the keyword 'bbox'
+        plt.text(-120, 56, print(message), fontsize = 10,
+                bbox = dict(facecolor = 'red', alpha = 0.5))
+        plt.plot(x, c = 'g')
+        return True
+    else:
+        return False
+
 def onclick(event):
     print('%s click: button=%d, xdata=%f, ydata=%f' %
           ('double' if event.dblclick else 'single', event.button,
@@ -73,10 +107,13 @@ def onclick(event):
     checkForFire(event.ydata, event.xdata, '2023-05-25', fire_df)
     print(checkForFire)
 
-#display results on the map/ next to it. noes not work yet 
-#plt.text(-120, 56, return onclick, fontsize = 22, 
-#    bbox = dict(facecolor = 'moccasin', alpha = 0.5))
-#plt.plot(c = 'g')
+x = np.arange(-120, 65, 0.05)
+
+#Adding text inside a rectangular box by using the keyword 'bbox'
+plt.text(-120, 56, , fontsize = 10,
+		bbox = dict(facecolor = 'red', alpha = 0.5))
+
+plt.plot(x, c = 'g')
 
 
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
