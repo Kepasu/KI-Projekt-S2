7b1a46ed48741b0e61c7ddd3bbda3f541d1eabcd
Nobody01234567 <125545591+Nobody01234567@users.noreply.github.com>
2023-06-22 09:34:33 +0200

    Update us_map.py
    Kann jetzt auch die Daten der checkForFire Funktion auf der Karte ausgeben

--- us_map.py
+++ us_map.py
@@ -1,19 +1,14 @@
-import os 
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.ticker import ScalarFormatter
 import seaborn as sns
 import geopandas as gpd
 from shapely.geometry import Point
 import earthpy as et
-import geodatasets
 import arrow
-#from searchdata import checkForFire
 
-#weather_df = pd.read_csv('/Users/emilychristians/desktop/data/weather/city_info.csv')
 fire_df = pd.read_csv('/Users/emilychristians/desktop/ki_projekt/data/modis_2021_United_States.csv')
-#weather_df = pd.read_csv('/Users/emilychristians/desktop/ki_projekt/data/weather/city_info.csv')
 
 # Set the path to the 'city_info.csv' file
 city_info_path = '/Users/emilychristians/desktop/ki_projekt/data/weather/city_info.csv'
@@ -30,14 +25,10 @@ data = et.data.get_data('spatial-vector-lidar')
 os.chdir(os.path.join(et.io.HOME, 'earth-analytics'))
 
 # Import world boundary shapefile
-worldBound_path = os.path.join("data", "spatial-vector-lidar", "global", 
-                               "ne_110m_land", "ne_110m_land.shp", "/Users/emilychristians/desktop/ki_projekt/data/cb_2014_us_state_500k/cb_2014_us_state_500k.shp")
+worldBound_path = os.path.join("data", "spatial-vector-lidar", "global",
+                               "ne_110m_land", "ne_110m_land.shp",
+                               "/Users/emilychristians/desktop/ki_projekt/data/cb_2014_us_state_500k/cb_2014_us_state_500k.shp")
 worldBound = gpd.read_file(worldBound_path)
-
-# Create numpy array of x,y point locations
-#longitude then latitude 
-#add_points = np.array([[-105.2519,   40.0274], 
-#                      [ -80.2102,   25.7839]])
 
 # Create a GeoDataFrame from the city_info_df DataFrame
 geometry = [Point(xy) for xy in zip(city_info_df['Lon'], city_info_df['Lat'])]
@@ -45,18 +36,8 @@ city_locations = gpd.GeoDataFrame(city_info_df, geometry=geometry)
 
 # Plot point locations
 fig, ax = plt.subplots(figsize=(10, 7))
-
-worldBound.plot(figsize=(10, 5), color='k',
-               ax=ax)
-
-# Add city locations
+worldBound.plot(figsize=(10, 5), color='k', ax=ax)
 city_locations.plot(ax=ax, color='red', marker='o', markersize=50)
-
-# Add city locations
-city_locations.plot(ax=ax, 
-                    color='red', # was originally springgreen
-                    marker='.',
-                    markersize=70)
 
 # Setup x y axes with labels and add graticules
 ax.set(xlabel="Longitude (Degrees)", ylabel="Latitude (Degrees)",
@@ -76,7 +57,7 @@ def haversine(lat1, lon1, lat2, lon2):
     R = 6371  # Radius of the earth in kilometers
     dLat = np.radians(lat2 - lat1)
     dLon = np.radians(lon2 - lon1)
-    a = np.sin(dLat/2)**2 + np.cos(np.radians(lat1)) * np.cos(np.radians(lat2)) * np.sin(dLon/2)**2
+    a = np.sin(dLat / 2) ** 2 + np.cos(np.radians(lat1)) * np.cos(np.radians(lat2)) * np.sin(dLon / 2) ** 2
     c = 2 * np.arcsin(np.sqrt(a))
     return R * c
 
@@ -84,48 +65,35 @@ def load_city_data(city_id):
     fpath = '/users/emilychristians/desktop/ki_projekt/data/weather/' + str(city_id) + '.csv'
     return pd.read_csv(fpath)
 
-def load_fire_data():
-    return pd.read_csv('/users/emilychristians/desktop/ki_projekt/data/modis_2021_United_States.csv')
-
 def checkForFire(lat, lon, date, fire_df):
     fire_df['distance'] = haversine(lat, lon, fire_df['latitude'], fire_df['longitude'])
     closest_fire = fire_df.loc[fire_df['distance'].idxmin()]
     closest_dist = closest_fire['distance']
     closest_date = arrow.get(closest_fire['acq_date'])
-    #delta = abs(closest_date - arrow.get(date))
-    if closest_dist < 20:# and delta.days < 10:
-        message = 'There has been a fire in this area. It occurred on ' + closest_fire['acq_date'] + ' and was ' + str(closest_dist) + ' km away from the specified coordinates. Coordinates: ' + str(closest_fire['latitude']) + ',' + str(closest_fire['longitude'])
-        print(message)
-        x = np.arange(-120, 65, 0.05)
-        #Adding text inside a rectangular box by using the keyword 'bbox'
-        plt.text(-120, 56, print(message), fontsize = 10,
-                bbox = dict(facecolor = 'red', alpha = 0.5))
-        plt.plot(x, c = 'g')
+    if closest_dist < 20:
+        message = 'There has been a fire in this area.\nIt occurred on ' + closest_fire[
+            'acq_date'] + ' and was \n' + str(closest_dist) + '\nkm away from the specified coordinates. \nCoordinates: ' + str(
+            closest_fire['latitude']) + '\n,' + str(closest_fire['longitude'])
+        if ax.texts:
+            ax.texts[0].remove()  # Remove previous annotation if exists
+        plt.annotate(message, xy=(closest_fire['longitude'], closest_fire['latitude']), xytext=(0, -10),
+                     textcoords='offset points', arrowprops=dict(facecolor='forestgreen', arrowstyle='->'),
+                     color='forestgreen', backgroundcolor='white', fontsize=10)  # Set fontsize to 10
+        fig.canvas.draw()
         return True
     else:
         return False
 
 def onclick(event):
+    # if ctrl pressed, show city data
+    # if event.ctrl ... get nearest city ... 
+    # if not, show fire data
     print('%s click: button=%d, xdata=%f, ydata=%f' %
           ('double' if event.dblclick else 'single', event.button,
-            event.xdata, event.ydata))
+           event.xdata, event.ydata))
     checkForFire(event.ydata, event.xdata, '2023-05-25', fire_df)
-    print(checkForFire)
 
-x = np.arange(-120, 65, 0.05)
-
-#Adding text inside a rectangular box by using the keyword 'bbox'
-plt.text(-120, 56, "Feuer" , fontsize = 10,
-		bbox = dict(facecolor = 'red', alpha = 0.5))
-
-plt.plot(x, c = 'g')
-
-
+# Connect the onclick function to the figure canvas
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
-
-# Add title and axes labels
-ax.set(title="World Map - Geographic Coordinate Reference System (long/lat degrees)",
-       xlabel="X Coordinates (latitude)",
-       ylabel="Y Coordinates (longitude)");
 
 plt.show()
